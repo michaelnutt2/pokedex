f8b79f4e965dab6b7dbf7e597b7ff2a305119d7b
Michael Nutt <michaeltnutt@gmail.com>
2026-04-08 15:54:58 +0000

    added some views

--- pokedex_project/core/urls.py
+++ pokedex_project/core/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('pokemon/', views.pokemon_list, name='pokemon_list'),
+    path('locations/', views.pokemon_locations, name='pokemon_locations'),
+    path('python/', views.python_logic, name='python_logic')
 ]
--- pokedex_project/core/views.py
+++ pokedex_project/core/views.py
@@ -4,10 +4,43 @@ from .models import Pokemon
 # Create your views here.
 def pokemon_list(request):
     # Fetch data
-    pokemon = Pokemon.objects.all().order_by('pokedex_number')[:50]
+    pokemon = Pokemon.objects.all().order_by('pokedex_number')[:10]
+    # SELECT * FROM pokemon WHERE hp > 100
+    tanks = Pokemon.objects.filter(hp__gt=100)
+
+    # Select * FROM pokemon WHERE name LIKE '%izard%'
+    izards = Pokemon.objects.filter(name__icontains='izard')
 
     context = {
-        'pokemon_list': pokemon
+        'pokemon_list': pokemon,
+        'tanks': tanks,
+        'izards': izards
     }
 
     return render(request, 'core/pokemon_list.html', context)
+
+def pokemon_locations(request):
+    # Fetch data
+    pokemon = Pokemon.objects.all().order_by('pokedex_number')[:10]
+
+    context = {
+        'pokemon_list': pokemon,
+    }
+
+    return render(request, 'core/locations.html', context)
+
+def python_logic(request):
+    # Fetch data
+    pokemon = Pokemon.objects.all()
+
+    if pokemon.count() > 1000:
+        message = "Warning: That's a lot of Pokemon!"
+    else:
+        message = "Whered did all the Pokemon go?"
+
+    context = {
+        'pokemon_list': pokemon[:10],
+        'message': message,
+    }
+
+    return render(request, 'core/python.html', context)
